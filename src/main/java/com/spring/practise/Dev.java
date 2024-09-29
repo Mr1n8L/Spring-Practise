785908376f83415e4c8e0e44dcfcebca62da414f
Mr1n8L <mrinaljani1822@gmail.com>
2024-09-29 16:49:36 +0530

    ApplicationContext

---
+++ src/main/java/com/spring/practise/Dev.java
@@ -0,0 +1,10 @@
+package com.spring.practise;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.println("Spring Boot Practise");
+
+    }
+}
